d330372a379dcd6e778e440b1ca6122619fa3f7e
Syafira Adiba <55017886+syafira1907@users.noreply.github.com>
2021-07-02 09:30:56 +0700

    Delete storage directory

--- storage/framework/views/04ac1e7fa487514fa4d781821944dd298f3ee3b4.php
+++
@@ -1,4 +0,0 @@
-<div class="footer-wrap pd-20 mb-20 card-box">
-    DeskApp - Bootstrap 4 Admin Template
-</div>
-<?php /**PATH C:\xampp\htdocs\testmodul6\resources\views/admin/layout/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/2ce62d486a8d2893e6c4acfe4854b16037d9e51d.php
+++
@@ -1,4 +0,0 @@
-<div class="footer-wrap pd-20 mb-20 card-box">
-    DeskApp - Bootstrap 4 Admin Template
-</div>
-<?php /**PATH C:\xampp\htdocs\coba_tubes\resources\views/admin/layout/footer.blade.php ENDPATH**/ ?>
